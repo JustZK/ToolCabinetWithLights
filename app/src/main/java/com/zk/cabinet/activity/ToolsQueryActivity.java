b4df343a7f92f988e9f9e0b505780119d0bd066a
JustZK <zk199519@gmail.com>
2019-07-15 20:49:19 +0800

    1、修改登录界面 2、添加删除工具功能

--- app/src/main/java/com/zk/cabinet/activity/ToolsQueryActivity.java
+++ app/src/main/java/com/zk/cabinet/activity/ToolsQueryActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -35,6 +36,11 @@ public class ToolsQueryActivity extends TimeOffAppCompatActivity implements View
 
     private List<Cabinet> cabinetList;
     private String[] boxName;
+
+    private Spinner dialog_add_box_name_sp;
+    private EditText dialog_add_tools_epc_edt, dialog_add_tools_name_edt, dialog_add_tools_light_edt;
+    private AlertDialog.Builder builder;
+    private ArrayAdapter<String> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +74,11 @@ public class ToolsQueryActivity extends TimeOffAppCompatActivity implements View
         for (int i = 0; i < boxNameList.size(); i++) {
             boxName[i] = boxNameList.get(i);
         }
+
+        binding.toolsQueryLv.setOnItemClickListener(onItemClickListener);
+
+        spinnerAdapter = new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item, boxName);
     }
 
 
@@ -86,24 +97,30 @@ public class ToolsQueryActivity extends TimeOffAppCompatActivity implements View
         switch (v.getId()) {
             case R.id.tools_add_fab:
                 mDialogView = getLayoutInflater().inflate(R.layout.dialog_add_tool, null);
-                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
-                        R.layout.support_simple_spinner_dropdown_item, boxName);
-                ((Spinner) mDialogView.findViewById(R.id.dialog_add_box_name_sp)).setAdapter(spinnerAdapter);
-                new AlertDialog.Builder(ToolsQueryActivity.this)
-                        .setView(mDialogView)
-                        .setTitle(getString(R.string.tools_add))
+//                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
+//                        R.layout.support_simple_spinner_dropdown_item, boxName);
+                dialog_add_box_name_sp = mDialogView.findViewById(R.id.dialog_add_box_name_sp);
+                dialog_add_tools_epc_edt = mDialogView.findViewById(R.id.dialog_add_tools_epc_edt);
+                dialog_add_tools_name_edt = mDialogView.findViewById(R.id.dialog_add_tools_name_edt);
+                dialog_add_tools_light_edt = mDialogView.findViewById(R.id.dialog_add_tools_light_edt);
+                dialog_add_box_name_sp.setAdapter(spinnerAdapter);
+                builder = new AlertDialog.Builder(ToolsQueryActivity.this);
+                builder.setView(mDialogView);
+
+                dialog_add_tools_epc_edt.setEnabled(true);
+                dialog_add_tools_light_edt.setEnabled(true);
+                dialog_add_box_name_sp.setEnabled(true);
+                builder.setTitle(getString(R.string.tools_add))
                         .setNegativeButton(getString(R.string.cancel), null)
+//                        .setNegativeButton(null, null)
                         .setPositiveButton(getString(R.string.sure), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                String epc = ((EditText) mDialogView.
-                                        findViewById(R.id.dialog_add_tools_epc_edt)).
+                                String epc = dialog_add_tools_epc_edt.
                                         getText().toString().trim();
-                                String name = ((EditText) mDialogView.
-                                        findViewById(R.id.dialog_add_tools_name_edt)).
+                                String name = dialog_add_tools_name_edt.
                                         getText().toString().trim();
-                                String toolLightNumber = ((EditText) mDialogView.
-                                        findViewById(R.id.dialog_add_tools_light_edt)).
+                                String toolLightNumber = dialog_add_tools_light_edt.
                                         getText().toString().trim();
                                 if (!TextUtils.isEmpty(epc) &&
                                         !TextUtils.isEmpty(name) &&
@@ -112,8 +129,8 @@ public class ToolsQueryActivity extends TimeOffAppCompatActivity implements View
 
                                     Tools tools = new Tools();
 
-                                    String boxName = ((Spinner) mDialogView.findViewById(R.id.dialog_add_box_name_sp)).getSelectedItem().toString();
-                                    for (int p = 3; p < cabinetList.size(); p++) {
+                                    String boxName = dialog_add_box_name_sp.getSelectedItem().toString();
+                                    for (int p = 0; p < cabinetList.size(); p++) {
                                         if (boxName.equals(cabinetList.get(p).getBoxName())) {
                                             tools.setCellNumber(cabinetList.get(p).getCellNumber());
                                         }
@@ -136,4 +153,84 @@ public class ToolsQueryActivity extends TimeOffAppCompatActivity implements View
                 break;
         }
     }
+
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+            mDialogView = getLayoutInflater().inflate(R.layout.dialog_add_tool, null);
+
+            dialog_add_box_name_sp = mDialogView.findViewById(R.id.dialog_add_box_name_sp);
+            dialog_add_tools_epc_edt = mDialogView.findViewById(R.id.dialog_add_tools_epc_edt);
+            dialog_add_tools_name_edt = mDialogView.findViewById(R.id.dialog_add_tools_name_edt);
+            dialog_add_tools_light_edt = mDialogView.findViewById(R.id.dialog_add_tools_light_edt);
+            dialog_add_box_name_sp.setAdapter(spinnerAdapter);
+            builder = new AlertDialog.Builder(ToolsQueryActivity.this);
+            builder.setView(mDialogView);
+
+            dialog_add_tools_epc_edt.setEnabled(false);
+            dialog_add_tools_light_edt.setEnabled(false);
+            dialog_add_box_name_sp.setEnabled(false);
+            dialog_add_tools_epc_edt.setText(list.get(position).getEpc());
+            dialog_add_tools_name_edt.setText(list.get(position).getToolName());
+            dialog_add_tools_light_edt.setText(String.valueOf(list.get(position).getToolLightNumber()));
+            String bn = CabinetService.getInstance().queryEq(list.get(position)
+                    .getCellNumber()).getBoxName();
+            for (int p = 0; p < boxName.length; p++) {
+                if (bn.equals(boxName[p])) {
+                    dialog_add_box_name_sp.setSelection(p);
+                    break;
+                }
+            }
+            builder.setTitle("工具修改")
+                    .setNegativeButton(getString(R.string.cancel), null)
+                    .setNeutralButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ToolsService.getInstance().delete(list.get(position));
+                            list = ToolsService.getInstance().loadAll();
+                            mAdapter.setList(list);
+                            mAdapter.notifyDataSetChanged();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setPositiveButton(getString(R.string.sure), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            String epc = dialog_add_tools_epc_edt.
+                                    getText().toString().trim();
+                            String name = dialog_add_tools_name_edt.
+                                    getText().toString().trim();
+                            String toolLightNumber = dialog_add_tools_light_edt.
+                                    getText().toString().trim();
+                            if (!TextUtils.isEmpty(epc) &&
+                                    !TextUtils.isEmpty(name) &&
+                                    !TextUtils.isEmpty(toolLightNumber) &&
+                                    RegularExpressionUtil.isNumber(toolLightNumber)) {
+
+                                Tools tools = list.get(position);
+
+//                                String boxName = ((Spinner) mDialogView.findViewById(R.id.dialog_add_box_name_sp)).getSelectedItem().toString();
+//                                for (int p = 0; p < cabinetList.size(); p++) {
+//                                    if (boxName.equals(cabinetList.get(p).getBoxName())) {
+//                                        tools.setCellNumber(cabinetList.get(p).getCellNumber());
+//                                    }
+//                                }
+//                                tools.setEpc(epc);
+                                tools.setToolName(name);
+//                                tools.setToolState(0);
+//                                tools.setToolLightNumber(Integer.parseInt(toolLightNumber));
+                                ToolsService.getInstance().insertOrUpdate(tools);
+                                list = ToolsService.getInstance().loadAll();
+                                mAdapter.setList(list);
+                                mAdapter.notifyDataSetChanged();
+                            } else {
+                                showToast(getText(R.string.fill_complete));
+                            }
+                        }
+                    })
+                    .setCancelable(false)
+                    .show();
+        }
+    };
 }
